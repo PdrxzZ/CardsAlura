707d1f47dc238abab330e3d894636bfbddf7f0ed
Pedro Loli <00001099363330SP@al.educacao.sp.gov.br>
2024-12-03 00:44:57 -0300

    Add files via upload

---
+++ respostas.js
@@ -0,0 +1,105 @@
+criaCartao('Pokémon e Fraquezas', 'Qual tipo é forte contra o tipo Água?', 'Tipo Elétrico e Planta.')
+
+criaCartao('Pokémon e Fraquezas', 'Pokémon do tipo Pedra são fracos contra quais tipos?', 'Água, Planta, Lutador, Aço, e Terra.  ')
+
+criaCartao('Pokémon e Fraquezas', 'Qual tipo não afeta Pokémon do tipo Dragão?', 'Tipo Fada. ')
+
+criaCartao('Pokémon e Fraquezas', 'Qual é o único tipo que não tem fraqueza?', 'Tipo Fantasma contra Normal e vice-versa, mas não causam dano.  ')
+
+criaCartao('Pokémon e Fraquezas', 'Quais tipos têm resistência ao tipo Elétrico?', 'Elétrico, Planta, e Dragão. ')
+
+criaCartao('Pokémon e Fraquezas', 'Qual tipo é imune a ataques do tipo Terra?', 'Tipo Voador.')
+
+criaCartao('Pokémon e Fraquezas', 'Ataques do tipo Psíquico são fortes contra qual tipo?', ' Lutador e Veneno.  ')
+
+criaCartao('Pokémon e Fraquezas', 'Qual tipo de Pokémon é imune ao tipo Fantasma?', 'Tipo Normal.  ')
+
+criaCartao('Pokémon e Fraquezas', 'Qual tipo de Pokémon tem fraqueza a si mesmo?', 'Tipo Dragão. ')
+
+criaCartao('Pokémon e Fraquezas', 'Quais tipos são fracos contra Pokémon do tipo Gelo?', 'Dragão, Terra, Voador e Planta. ')
+
+
+criaCartao( 'Evolução ', 'Quantas evoluções Eevee pode ter atualmente?', 'Oito.')
+
+criaCartao( 'Evolução ', 'Qual Pokémon evolui usando uma Pedra do Fogo?', 'Growlithe, Vulpix, Eevee (para Flareon), entre outros. ')
+
+criaCartao( 'Evolução ', 'Qual Pokémon evolui segurando uma Razor Fang à noite?', ' Gligar para Gliscor.')
+
+criaCartao( 'Evolução ', 'Como Magneton evolui para Magnezone?', 'Subindo de nível em um lugar com forte magnetismo.  ')
+
+criaCartao( 'Evolução ', 'Qual é a pré-evolução de Lucario?', 'Riolu.')
+
+criaCartao( 'Evolução ', 'Qual é a condição para evoluir um Pancham em Pangoro?', 'Subir de nível com um Pokémon do tipo Sombrio na equipe.  ')
+
+criaCartao( 'Evolução ', 'Qual Pokémon só evolui após aprender o movimento Ancient Power?', 'Piloswine para Mamoswine.')
+
+criaCartao( 'Evolução ', 'Qual Pokémon evolui trocando enquanto segura o item Kings Rock?', ' Poliwhirl para Politoed e Slowpoke para Slowking.  ')
+
+criaCartao( 'Evolução ', 'Qual Pokémon evolui de Goomy para Sliggoo e de Sliggoo para Goodra?', 'Sliggoo evolui com chuva ou neblina no nível 50.  ')
+
+criaCartao( 'Evolução ', 'Como Rockruff pode evoluir para Lycanroc em suas formas diferentes?', ' Depende do horário do dia: dia para Forma Diurna, noite para Forma Noturna, ou crepúsculo para Forma Crepuscular. ')
+
+criaCartao('Regiões Pokémon', 'Em qual região os jogos Pokémon Red e Blue acontecem?', 'Kanto.  ')
+
+criaCartao('Regiões Pokémon', 'Qual é a região baseada no Japão feudal introduzida em Pokémon Legends: Arceus?', 'Hisui.  ')
+
+criaCartao('Regiões Pokémon', 'Qual região apresenta os starters Snivy, Tepig e Oshawott?', 'Unova.')
+
+criaCartao('Regiões Pokémon', 'Em qual região é ambientado o anime Pokémon XY?', ' Kalos.')
+
+criaCartao('Regiões Pokémon', 'Qual é a região de Pokémon Diamond e Pearl?', ' Sinnoh.')
+
+criaCartao('Regiões Pokémon', 'Qual região é inspirada no Reino Unido?', 'Galar.  ')
+
+criaCartao('Regiões Pokémon', 'Qual região tem como destaque um arquipélago tropical?', ' Alola.  ')
+
+criaCartao('Regiões Pokémon', 'Qual região foi a primeira a introduzir Mega Evoluções?', ' Kalos.')
+
+criaCartao('Regiões Pokémon', 'Qual região é apresentada em Pokémon Scarlet e Violet?', 'Paldea.  ')
+
+criaCartao('Regiões Pokémon', 'Qual é a região natal de Pikachu?', ' Kanto.  ')
+
+criaCartao('Lendários e Míticos', 'Qual Pokémon Lendário controla o espaço?', 'Palkia.  ')
+
+criaCartao('Lendários e Míticos', 'Quem são os três pássaros lendários de Kanto?', ' Articuno, Zapdos e Moltres. ')
+
+criaCartao('Lendários e Míticos', 'Qual Pokémon Lendário aparece no topo da Torre Campana em Johto?', ' Ho-Oh.  ')
+
+criaCartao('Lendários e Míticos', 'Qual é o Pokémon Lendário de Alola associado ao sol?', 'Solgaleo.  ')
+
+criaCartao('Lendários e Míticos', 'Qual Pokémon Mítico é conhecido como o "Pai de todos os Pokémon"?', 'Arceus.  ')
+
+criaCartao('Lendários e Míticos', 'Qual Pokémon Lendário pode alterar o clima em batalha?', 'Kyogre, Groudon e Rayquaza.  ')
+
+criaCartao('Lendários e Míticos', 'Qual Pokémon Mítico pode se transformar em outros Pokémon?', ' Mew.  ')
+
+criaCartao('Lendários e Míticos', 'Qual Pokémon Lendário tem o poder de viajar no tempo?', 'Celebi.  ')
+
+criaCartao('Lendários e Míticos', 'Qual Pokémon Lendário foi introduzido em Pokémon Sword e Shield?', 'Zacian e Zamazenta.  ')
+
+criaCartao('Lendários e Míticos', 'Qual Pokémon Lendário aparece em Pokémon Emerald na Sky Pillar?', 'Rayquaza.  ')
+
+criaCartao('Curiosidades Gerais', 'Qual Pokémon é conhecido como o "Pokémon Mouse"?', 'Pikachu.  ')
+
+criaCartao('Curiosidades Gerais', 'Quantos Pokémon existiam originalmente na primeira geração?', '151. ')
+
+criaCartao('Curiosidades Gerais', 'Qual é o Pokémon mais leve conhecido?', 'Qual é o Pokémon mais leve conhecido?')
+
+criaCartao('Curiosidades Gerais', 'Qual é o maior Pokémon em termos de altura?', 'Eternatus (20 m na forma Eternamax).')
+
+criaCartao('Curiosidades Gerais', 'Qual foi o primeiro Pokémon criado, segundo os desenvolvedores?', 'Rhydon.')
+
+criaCartao('Curiosidades Gerais', 'Qual Pokémon pode se comunicar com humanos falando diretamente?', 'Meowth do anime e Mewtwo.')
+
+criaCartao('Curiosidades Gerais', 'Qual é o primeiro Pokémon na Pokédex Nacional?', ' Bulbasaur.')
+
+criaCartao('Curiosidades Gerais', 'Qual Pokémon aparece na capa do jogo Pokémon Gold?', ' Ho-Oh.')
+
+criaCartao('Curiosidades Gerais', 'Quantos tipos de Pokémon existem atualmente?', '18.')
+
+criaCartao('Curiosidades Gerais', 'Qual Pokémon é conhecido por cantar para colocar os outros para dormir?', 'Jigglypuff.')
+
+
+
+
+
